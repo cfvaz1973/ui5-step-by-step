9bde80d91f0691daf3769d628b385f33d2222481
Carlos Vaz <cfvaz1973@gmail.com>
2023-04-28 17:22:00 +0100

    28.04.2023(1)

--- webapp/component.js
+++ webapp/component.js
@@ -2,7 +2,7 @@ sap.ui.define(
   ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
   function (UIComponent, JSONModel) {
     "use strict";
-    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+    return UIComponent.extend("sap.ui.demo.walkthrough.component", {
       // metadata section defines a reference to the root view
       metadata: {
         manifest: "json", // metadata points to manifest.json file
