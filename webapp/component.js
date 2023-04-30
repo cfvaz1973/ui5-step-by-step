d7d621def2db8d1cbca2d78521b1eb636eeaa01a
cfvaz1973 <cfvaz1973@gmail.com>
2023-04-30 17:11:09 +0100

    30.04.2023

--- webapp/component.js
+++ webapp/component.js
@@ -2,26 +2,24 @@ sap.ui.define(
   ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
   function (UIComponent, JSONModel) {
     "use strict";
+
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-      // metadata section defines a reference to the root view
       metadata: {
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        manifest: "json", // metadata points to manifest.json file
+        manifest: "json",
       },
-      // init function will instantiate the data model and i18n model
-      // models are now set directly on the component and not on the root view
-      // However, as nested controls automatically inherit the models from the parent controls,
-      // the models will be available on the view as well
+
       init: function () {
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
+
         // set data model
-        let oData = {
+        var oData = {
           recipient: {
             name: "World",
           },
         };
-        let oModel = new JSONModel(oData);
+        var oModel = new JSONModel(oData);
         this.setModel(oModel);
       },
     });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,20 +2,18 @@ sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
   function (Controller, MessageToast) {
     "use strict";
+
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onShowHello: function () {
         // read msg from i18n model
-        let oBundle = this.getView().getModel("i18n").getResourceBundle();
-        let sRecipient = this.getView()
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
           .getModel()
           .getProperty("/recipient/name");
-        let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
         // show message
         MessageToast.show(sMsg);
-        // 1.show a native JavaScript alert
-        // alert("Hello World");
-        // 2.show message with message toast
-        // MessageToast.show("Hello World");
       },
     });
   }
