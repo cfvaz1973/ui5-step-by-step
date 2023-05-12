98b7f6aa4a0e627efda66845ed87f7e27b475b12
cfvaz1973 <cfvaz1973@gmail.com>
2023-05-12 18:56:00 +0100

    12-05-2023

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,35 @@
+sap.ui.define(
+  ["sap/ui/core/util/MockServer", "sap/base/util/UriParameters"],
+  function (MockServer, UriParameters) {
+    "use strict";
+
+    return {
+      init: function () {
+        // create
+        //           rootUri: "https://services.odata.org/V2/Northwind/Northwind.svc/",
+        var oMockServer = new MockServer({
+          rootUri: "/odata/V2/Northwind/Northwind.svc/",
+        });
+
+        var oUriParameters = new UriParameters(window.location.href);
+
+        // configure mock server with a delay
+        MockServer.config({
+          autoRespond: true,
+          autoRespondAfter: oUriParameters.get("serverDelay") || 500,
+        });
+
+        // simulate
+        var sPath = sap.ui.require.toUrl(
+          "sap/ui/demo/walkthrough/localService"
+        );
+        oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
+
+        // start
+        oMockServer.start();
+
+        console.log("Running the app with mock data");
+      },
+    };
+  }
+);
